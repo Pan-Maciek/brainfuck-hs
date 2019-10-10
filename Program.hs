02c431caba535b0c6b5873d4cd7a7e8e71c0519f
koziejka <koziejka.com@gmail.com>
2019-10-10 20:51:03 +0200

    Implemented optimizer

--- Program.hs
+++ Program.hs
@@ -1,4 +1,5 @@
 import Data.List
+import System.IO
 
 data Instruction =
   Add Int | Move Int
@@ -7,27 +8,37 @@ data Instruction =
   | Program [Instruction]
   deriving (Show)
 
-parser :: ([Instruction], [Char]) -> ([Instruction], [Char])
-parser (acc, ('+':xs)) = parser (Add 1 : acc, xs)
-parser (acc, ('-':xs)) = parser (Add (-1) : acc, xs)
-parser (acc, ('>':xs)) = parser (Move 1 : acc, xs)
-parser (acc, ('<':xs)) = parser (Move (-1) : acc, xs)
-parser (acc, ('.':xs)) = parser (Print : acc, xs)
-parser (acc, (',':xs)) = parser (Read : acc, xs)
-parser (acc, ('[':xs)) = parser (Loop instructions : acc, tail)
-  where (instructions, tail) = parser ([], xs)
-parser (acc, (']':xs)) = (acc, xs)
-parser (acc, _:xs) = parser (acc, xs) -- skip comments
-parser (acc, []) = (acc, []) 
-
 parse :: [Char] -> Instruction
-parse input = Program tokens
-  where (tokens,_) = parser ([], input)
+parse code = program where 
+  parser (acc, ('+':xs)) = parser (Add 1 : acc, xs)
+  parser (acc, ('-':xs)) = parser (Add (-1) : acc, xs)
+  parser (acc, ('>':xs)) = parser (Move 1 : acc, xs)
+  parser (acc, ('<':xs)) = parser (Move (-1) : acc, xs)
+  parser (acc, ('.':xs)) = parser (Print : acc, xs)
+  parser (acc, (',':xs)) = parser (Read : acc, xs)
+  parser (acc, ('[':xs)) = parser (Loop instructions : acc, tail)
+    where (instructions, tail) = parser ([], xs)
+  parser (acc, (']':xs)) = (reverse acc, xs)
+  parser (acc, _:xs) = parser (acc, xs) -- skip comments
+  parser (acc, []) = ([Program (reverse acc)], []) 
 
-compile :: Instruction -> String
-compile (Add val) = "data[ptr] = (data[ptr] + " ++ show (val + 256) ++ ") % 256;"
+  (program: _, _) = parser ([], code)
+
+optimize :: Instruction -> Instruction
+optimize program = optimized where
+  optimize (Add n: Add m: xs) = optimize (Add (m + n) : xs)
+  optimize (Move n: Move m: xs) = optimize (Move (m + n) : xs)
+  optimize ((Loop instructions): xs) = (Loop (optimize instructions)) : (optimize xs)
+  optimize ((Program instructions): xs) = [Program (optimize instructions)]
+  optimize (instr: xs) = instr : (optimize xs)
+  optimize [] = []
+  optimized:_ = optimize [program]
+
+compile :: Instruction -> [Char]
+compile (Add val) = "data[ptr] = (data[ptr] + " ++ show (val `mod` 256) ++ ") % 256;"
 compile (Move val) = "ptr +=" ++ show val ++ ";"
-compile Print = "print (data[ptr])"
-compile Read = "data[ptr] = await read ()"
+compile Print = "print (data[ptr]);"
+compile Read = "data[ptr] = await read ();"
 compile (Loop instructions) = "while (data[ptr]) { " ++ intercalate "\n" (map compile instructions) ++ " }"
-compile (Program instructions) = "async function program(print, read) { const data = Array.from({length:30000},()=>0);let ptr=0; " ++ intercalate "\n" (map compile instructions) ++ " }"
+compile (Program instructions) = "async function program(print, read) { " ++ runtime ++ intercalate "\n" (map compile instructions) ++ " }" where
+  runtime = "const data = Array.from({ length: 30000 }, () => 0); let ptr = 0;"
