105bdf185882445a241658f894901ce7351b1c8a
koziejka <koziejka.com@gmail.com>
2019-10-10 22:06:37 +0200

    Implemented basic cli interface.

--- Program.hs
+++ Program.hs
@@ -1,5 +1,7 @@
 import Data.List
 import System.IO
+import System.Environment
+import Text.Regex
 
 data Instruction =
   Add Int | Move Int
@@ -26,7 +28,7 @@ parse code = program where
 
 optimize :: Instruction -> Instruction
 optimize program = optimized where
-  optimize (Add n: Add m: xs) =  optimize (if m + n /= 0 then (Add (m + n) : xs) else xs)
+  optimize (Add n: Add m: xs) =  optimize (if m + n /= 0 then Add (m + n) : xs else xs)
   optimize (Move n: Move m: xs) = optimize (Move (m + n) : xs)
   optimize ((Loop instructions): xs) = (Loop (optimize instructions)) : (optimize xs)
   optimize ((Program instructions): xs) = [Program (optimize instructions)]
@@ -44,4 +46,19 @@ compile Read = "scanf(\"%c\", data + ptr);"
 compile (Loop instructions) = "while (data[ptr]) { " ++ intercalate "\n" (map compile instructions) ++ " }"
 compile (Program instructions) = runtime ++ "int main() { " ++ intercalate "\n" (map compile instructions) ++ " }" where
   memSize = 30000
-  runtime = "#include <stdio.h>\n#define MEM_SIZE = " ++ show memSize ++ "\nchar data[MEM_SIZE];"
+  runtime = "#include <stdio.h>\n#define MEM_SIZE " ++ show memSize ++ "\nchar data[MEM_SIZE]; int ptr = 0;"
+
+main = do
+  args <- getArgs
+  run args
+
+compileFile fileIn fileOut = do
+  code <- readFile fileIn
+  writeFile fileOut (compile (optimize (parse code)))
+
+run (fileIn:"-o":fileOut:[]) = compileFile fileIn fileOut 
+
+run (fileIn:[]) = compileFile fileIn fileOut where
+    fileOut = subRegex (mkRegex "\\.\\bf$") fileIn ".c"
+
+run (_) = print "Invalid arguments provided."
